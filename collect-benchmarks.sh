386ad9045cc8659b33ccec7218b63afeb6e7533c
Jared Wasinger <j-wasinger@hotmail.com>
2023-01-14 17:20:14 -0500

    update

--- collect-benchmarks.sh
+++ collect-benchmarks.sh
@@ -10,11 +10,11 @@ echo "running asm384 go benchmarks"
 (cd evmmax-arith-little-endian/ && go test -timeout=4h -run=^$ -bench=Op/"asm384_.*384") > $benchmark_dir/go-arith-benchmarks-asm384.txt
 
 echo "running asm384 evm benchmarks"
-(cd evm-benchmarks-asm384/evmmax_generator && python3 generate.py MULMONTX 6 6) > $benchmark_dir/geth-evm-asm384.csv
-(cd evm-benchmarks-asm384/evmmax_generator && python3 generate.py ADDMODX 6 6) >> $benchmark_dir/geth-evm-asm384.csv
-(cd evm-benchmarks-asm384/evmmax_generator && python3 generate.py SUBMODX 6 6) >> $benchmark_dir/geth-evm-asm384.csv
+(cd evm-benchmarks-asm384/evmmax_generator && python3 benchmark.py MULMONTX 6 6) > $benchmark_dir/geth-evm-asm384.csv
+(cd evm-benchmarks-asm384/evmmax_generator && python3 benchmark.py ADDMODX 6 6) >> $benchmark_dir/geth-evm-asm384.csv
+(cd evm-benchmarks-asm384/evmmax_generator && python3 benchmark.py SUBMODX 6 6) >> $benchmark_dir/geth-evm-asm384.csv
 
 echo "running little-endian benchmarks"
-(cd evm-benchmarks-le/evmmax_generator && python3 generate.py) > $benchmark_dir/geth-evm-little-endian.csv
+(cd evm-benchmarks-le/evmmax_generator && python3 benchmark.py) > $benchmark_dir/geth-evm-little-endian.csv
 echo "running big-endian EVM benchmarks"
-(cd evm-benchmarks-eip-5843/evmmax_generator && python3 generate.py) > $benchmark_dir/geth-evm-eip-5843.csv
+(cd evm-benchmarks-eip-5843/evmmax_generator && python3 benchmark.py) > $benchmark_dir/geth-evm-eip-5843.csv
