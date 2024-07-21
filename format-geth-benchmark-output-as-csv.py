ad5bb1c79b223cd08d5b794fa19a6451d33c963a
Jared Wasinger <j-wasinger@hotmail.com>
2024-07-21 07:41:50 +0000

    update

--- format-geth-benchmark-output-as-csv.py
+++ format-geth-benchmark-output-as-csv.py
@@ -1,12 +1,18 @@
 import sys, math, re
 
-input_lines = sys.stdin.readlines()
+# input_lines = sys.stdin.readlines()
 
-for line in input_lines[4:-2]:
-    parts = [s for s in line.split(' ') if s]
-    name = parts[0].split('/')[1].split('_')[1]
-    limbs = re.sub(r'#.*$', '', parts[0].split('/')[1].split('_')[2])
-    limbs = re.sub(r'-.*$', '', limbs)
-    limbs = int(int(limbs) / 64)
-    time = math.ceil(float(parts[-2]))
-    print("{},{},{}".format(name,limbs,time))
+with open('geth-benchmark-output-example.txt', 'r') as f:
+    input_lines = f.readlines()
+    for line in input_lines[4:-2]:
+        parts = [s for s in line.split(' ') if s]
+
+        target_parts = parts[0][13:].split('-')[:-1]
+        op, width, preset = target_parts[0], target_parts[1], target_parts[2]
+
+        if parts[-1] != 'ns/op\n':
+            raise Exception("fuck")
+
+        time = math.ceil(float(parts[-2]))
+
+        print("{},{},{},{}".format(preset, op, width, time))
